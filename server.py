dede789b768fe1f7904db34979c20652a04bc34c
Your Name <you@example.com>
2020-02-06 12:20:25 -0800

    Completed register routes

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,42 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route('/users')
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+
+    return render_template("user_list.html", users=users)
+
+@app.route('/register', methods=["GET"])
+def register_form():
+    """Show registration form"""
+
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    return render_template("register_form.html", email=email,
+                                                 password=password)
+
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    """User email and password processed"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    if not User.query.filter(User.email == email).first():
+
+        new_user = User(email=email, password=password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
